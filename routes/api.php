fac205160d90fb423b54b34050e6caae55832ad8
janth96 <janth1996@gmail.com>
2021-05-16 23:19:32 +1200

    Install JWT
    - Created endpoints for login and register

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
-
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Hash;
+
+use App\Models\User;
 
 use App\Http\Controllers\ResumeController;
 
@@ -16,8 +18,32 @@ use App\Http\Controllers\ResumeController;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::middleware('auth')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/resumes', [ResumeController::class, 'index']);
+// Create a user
+Route::get('/user-create', function(Request $request) {
+  User::create([
+    'email' => "jan@jan.jan",
+    'name' => 'Jan',
+    'password' => Hash::make('mypassword'),
+  ]);
+});
+
+// Log in a user
+Route::post('/login', function(Request $request) {
+
+  $credentials = $request->only(['email', 'password']);
+
+  $token = auth()->attempt($credentials);
+
+  return $token;
+});
+
+// Get authenticated user
+Route::middleware('auth')->get('/me', function (){
+  return auth()->user();
+});
+
+// Route::get('/resumes', [ResumeController::class, 'index']);
