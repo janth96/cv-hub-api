66848019520d9d7b49ce2036737e364f485e6e8e
janth96 <janth1996@gmail.com>
2021-05-30 03:06:21 +1200

    Authorization error messages

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -25,10 +25,26 @@ class AuthController extends Controller
   }
 
   public function login (Request $request) {
-    $credentials = $request->only(['email', 'password']);
 
+    $user = User::where('email', $request['email'])->first();
+
+    // User not found
+    if(!$user) return response()->json(["message" => "user not found"], 404);
+
+    // Log in user
+    $credentials = $request->only(['email', 'password']);
     $token = auth()->attempt($credentials);
 
-    return $token;
+    // Return token
+    if($token) {
+      return response()->json(['token' => $token], 200);
+    }
+
+    // False credentials
+    return response()->json([
+      'data' => null,
+      'status' => "error",
+      'message' => "incorrect user-password combination",
+    ], 401);
   }
 }
